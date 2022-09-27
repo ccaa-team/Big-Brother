b4b088929a1f4b4662eac4044938ccd940ba28d3
notvirtio <tobich228@gmail.com>
2022-09-28 00:04:40 +0300

    Giant push of code

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,73 @@
-pub mod commands;
+use poise::{
+    async_trait,
+    serenity_prelude::{self as serenity, EventHandler, Message},
+};
 
-use crate::commands::*;
+use uwuifier::uwuify_str_sse;
 
-use std::env;
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
 
-use serenity::async_trait;
-use serenity::framework::StandardFramework;
-use serenity::framework::standard::macros::group;
-use serenity::prelude::*;
+struct Data {}
 
-#[group]
-#[commands(ping)]
-struct General;
+struct Handler {}
 
-struct Handler;
+static UWUCHANNEL: serenity::ChannelId = serenity::ChannelId(1024420622647967824);
+static NORMALCHANNEL: u64 = 1023332213078626448;
 
 #[async_trait]
-impl EventHandler for Handler {}
+impl EventHandler for Handler {
+    async fn message(&self, ctx: serenity::Context, msg: Message) {
+        let content = &msg.content_safe(&ctx.cache);
+        let channel_id = &msg.channel_id;
+
+        let author_name = match msg.author_nick(&ctx.http).await {
+            Some(x) => x,
+            None => msg.author.name,
+        };
+
+        if content == "" || channel_id.as_u64().to_owned() != NORMALCHANNEL {
+            return;
+        }
+
+        let uwud = uwuify_str_sse(content);
+
+        UWUCHANNEL
+            .send_message(&ctx.http, |msg| {
+                msg.embed(|e| {
+                    e.author(|a| a.name(author_name));
+                    e.description(uwud)
+                })
+            })
+            .await
+            .unwrap();
+    }
+}
+
+#[poise::command(prefix_command)]
+async fn help(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.send(|m| m.embed(|e| e.title("shit yorself"))).await?;
+
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() {
-    let framework = StandardFramework::new()
-        .configure(|c| c.prefix("'"))
-        .group(&GENERAL_GROUP);
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![help()],
+            prefix_options: poise::PrefixFrameworkOptions {
+                prefix: Some("'".into()),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .token(std::env::var("token").expect("No token in env"))
+        .intents(
+            serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT,
+        )
+        .user_data_setup(move |_, _, _| Box::pin(async move { Ok(Data {}) }))
+        .client_settings(|f| f.event_handler(Handler {}));
 
-    let token = env::var("token").expect("token");
-    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
-    let mut client = Client::builder(token, intents)
-        .event_handler(Handler)
-        .framework(framework)
-        .await
-        .expect("Error creating client");
-
-    if let Err(why) = client.start().await {
-        println!("Errored while running the client: {:?}", why)
-    }
+    framework.run().await.unwrap();
 }
