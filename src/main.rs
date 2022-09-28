39a66f6abcc4fa718805c543fc335bdc4816d6e0
notvirtio <tobich228@gmail.com>
2022-09-28 15:56:43 +0300

    Make uwuifier use a webhook

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
-use poise::serenity_prelude as serenity;
+use poise::serenity_prelude::{self as serenity, Webhook};
 
 use uwuifier::uwuify_str_sse;
 
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
-struct Data {}
+struct Data {
+    webhook: Webhook,
+}
 
-struct Handler {}
-
-static UWUCHANNEL: serenity::ChannelId = serenity::ChannelId(1024420622647967824);
 static NORMALCHANNEL: u64 = 1023332213078626448;
 
 async fn event_listener(
     ctx: &serenity::Context,
     event: &poise::Event<'_>,
     _framework: poise::FrameworkContext<'_, Data, Error>,
-    _user_data: &Data,
+    user_data: &Data,
 ) -> Result<(), Error> {
     match event {
         poise::Event::Message { new_message } => {
@@ -25,26 +24,30 @@ async fn event_listener(
             let content = &msg.content_safe(&ctx.cache);
             let channel_id = &msg.channel_id;
 
-            let author_name = msg.author.tag();
-
-            let avatar_url = match msg.author.avatar_url() {
-                Some(x) => x,
-                None => "".to_string(),
-            };
-
             if content != "" && channel_id.as_u64().to_owned() == NORMALCHANNEL {
+                let normal_name = &msg.author.name;
+
+                let author_name = uwuify_str_sse(&normal_name);
+
+                let avatar_url = match msg.author.avatar_url() {
+                    Some(x) => x,
+                    None => "".to_string(),
+                };
+
                 let uwud = uwuify_str_sse(content);
 
-                UWUCHANNEL
-                    .send_message(&ctx.http, |msg| {
-                        msg.embed(|e| {
-                            e.author(|a| a.name(author_name).icon_url(avatar_url));
-                            e.description(uwud);
-                            e.color(0xffffff)
-                        })
+                match user_data
+                    .webhook
+                    .execute(&ctx.http, false, |w| {
+                        w.avatar_url(avatar_url);
+                        w.username(author_name);
+                        w.content(uwud)
                     })
                     .await
-                    .unwrap();
+                {
+                    Ok(_) => (),
+                    Err(why) => println!("Error sending message: {:?}", why),
+                };
             }
         }
         _ => {}
@@ -53,15 +56,49 @@ async fn event_listener(
     Ok(())
 }
 
+#[poise::command(prefix_command)]
+async fn help(ctx: Context<'_>) -> Result<(), Error> {
+    match ctx
+        .send(|msg| {
+            msg.embed(|e| {
+                e.title("Big brother bot");
+                e.description("I uwuify text and will get rid of 🥹 when alex lets me")
+            })
+        })
+        .await
+    {
+        Ok(_) => (),
+        Err(why) => println!("Erro replying to help command: {:?}", why),
+    };
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() {
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
+            commands: vec![help()],
+            listener: |ctx, event, framework, user_data| {
+                Box::pin(event_listener(ctx, event, framework, user_data))
+            },
+            prefix_options: poise::PrefixFrameworkOptions {
+                prefix: Some("'".into()),
+                ..Default::default()
+            },
             ..Default::default()
         })
-        .token(std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN"))
-        .intents(serenity::GatewayIntents::non_privileged())
-        .user_data_setup(move |_ctx, _ready, _framework| Box::pin(async move { Ok(Data {}) }));
+        .token(std::env::var("token").expect("No token in env"))
+        .intents(
+            serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT,
+        )
+        .user_data_setup(move |ctx, _, _| {
+            Box::pin(async move {
+                Ok(Data {
+                    webhook: Webhook::from_id(&ctx.http, 1024655591270002718).await?,
+                })
+            })
+        });
 
     framework.run().await.unwrap();
 }
