be2f2c4101e6e184b83f0160bb833fb8d47ae6c5
notvirtio <tobich228@gmail.com>
2022-09-29 17:16:09 +0300

    Fix @everyone and add cache

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ require("dotenv").config();
 
 let uwuifier = new Uwuifier.default();
 
+let webhook_cache = {};
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
@@ -15,16 +17,22 @@ client.on("messageCreate", async msg => {
 
     let text = msg.cleanContent.slice(1, msg.cleanContent.length - 1)
 
+    text = text.replace("@everyone", "at everyone");
+
     if (text.length == 0) return;
 
     let uwu = uwuifier.uwuifySentence(text);
     let name = uwuifier.uwuifyWords(msg.author.username);
     let avatarUrl = msg.author.avatarURL();
 
-    let webhooks = await msg.channel.fetchWebhooks().catch(console.error);
+    let webhooks = webhook_cache[msg.channelId]
+    if (webhooks === undefined) {
+      webhooks = await msg.channel.fetchWebhooks().catch(console.error);
+      webhook_cache[msg.channelId] = webhooks;
+    }
     let webhook = webhooks.first();
 
-    await msg.delete();
+    await msg.delete().catch(console.err);
 
     webhook.send({
       content: uwu,
