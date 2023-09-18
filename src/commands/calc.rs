6d32884b2420adc38d1d55a59f7dca897412686e
viwty <viwty@proton.me>
2023-09-18 23:39:06 +0300

    i forgor

--- src/commands/calc.rs
+++ src/commands/calc.rs
@@ -1,21 +1,21 @@
-use mexprp::Config;
 use poise::command;
 
 use crate::{Context, Error};
 
-static CONFIG: Config = Config {
-    implicit_multiplication: false,
-    precision: 1024,
-    sqrt_both: true,
-};
+fn meth(expr: String) -> String {
+    let mut ctx: mexprp::Context<f64> = mexprp::Context::new();
+    ctx.cfg.precision = 1024;
+
+    match mexprp::eval_ctx(&expr, &ctx) {
+        Ok(r) => r.to_string(),
+        Err(e) => e.to_string(),
+    }
+}
 
 #[command(slash_command, prefix_command, track_edits)]
 /// Believe it or not, this is a calculator command
 pub async fn calc(ctx: Context<'_>, #[rest] expr: String) -> Result<(), Error> {
-    let result = match mexprp::eval::<f64>(&expr) {
-        Ok(r) => r.to_string(),
-        Err(e) => e.to_string(),
-    };
+    let result = meth(expr);
 
     ctx.send(|m| m.content(result).reply(true).ephemeral(true))
         .await?;
