860a077240e2c51c7131318de74d761b6edae9de
viwty <viwty@proton.me>
2024-01-19 19:12:44 +0300

    update to nixpkgs-unstable
    i'm not actually sure if the flake builds or not :KEKW:

--- flake.nix
+++ flake.nix
@@ -1,14 +1,17 @@
 {
   description = "Autovirt";
 
-  inputs = { nixpkgs.url = "nixpkgs"; };
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, rust-overlay }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
       version = "0.1.0";
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+      overlays = [ (import rust-overlay) ];
+      nixpkgsFor =
+        forAllSystems (system: import nixpkgs { inherit system overlays; });
     in {
       packages = forAllSystems (system:
         let
@@ -19,16 +22,31 @@
             src = ./.;
 
             #cargoSha256 = pkgs.lib.fakeSha256;
-            cargoSha256 = "sha256-r7TCEgbiDhRoX/BH+PicAhay36ecy42gEP0ezrYEw90=";
+            cargoSha256 = "sha256-U/vzbNAAQvlBF9UZTmZ5/t7tUPHE3Hnc/ZkW8eQoAac=";
           };
           env = builtins.readFile ./.env;
-        in { inherit autovirt;
-        default = pkgs.dockerTools.buildImage {
-          name = "autovirt";
-          contents = [ autovirt pkgs.cacert ];
-          config = {
-            Env = pkgs.lib.splitString "\n" env;
-            Cmd = [ "${autovirt}/bin/autovirt" ];
+        in {
+          inherit autovirt;
+          default = pkgs.dockerTools.buildImage {
+            name = "autovirt";
+            tag = "latest";
+            copyToRoot = pkgs.buildEnv {
+              name = "autovirt-root";
+              paths = [ autovirt pkgs.cacert ];
+              pathsToLink = [ "/bin" "/etc" ];
+            };
+            config = {
+              Env = pkgs.lib.splitString "\n" env;
+              Cmd = [ "/bin/autovirt" ];
+              ExposedPorts = { "5432" = { }; };
+            };
+          };
+        });
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [rust-bin.stable.latest.default];
           };
         });
     };
